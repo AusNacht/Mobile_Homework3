2ba7cf03bf59eb666da48b8a6faa282fad655bf1
Aaron Lathrop <ausnacht@gmail.com>
2013-06-07 04:06:38 -0400

    Signed-off-by: Aaron Lathrop <ausnacht@gmail.com>

---
+++ src/edu/fsu/cs/alathrop/homework3/MyWebFragment.java
@@ -0,0 +1,17 @@
+package edu.fsu.cs.alathrop.homework3;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MyWebFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle state)
+	{
+		return inflater.inflate(R.layout.activity_my_web_fragment, null, false);
+	}
+
+}
---
+++ src/edu/fsu/cs/alathrop/homework3/UrlListFragment.java
@@ -0,0 +1,18 @@
+package edu.fsu.cs.alathrop.homework3;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class UrlListFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle state)
+	{
+
+		return inflater.inflate(R.layout.activity_url_list_fragment, null, false);
+	}
+
+}
